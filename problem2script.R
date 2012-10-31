43cac1b20af7bc9ec046e15e45b49c8173c03b1d
Steven <steveninthemiddle@yahoo.com>
2012-10-31 17:41:54 -0400

    Fixed problem2script.R, generated new problem2.RData, Created Zip File

--- problem2script.R
+++ problem2script.R
@@ -1,23 +1,6 @@
 mydata=read.csv("problem2DownloadedData.csv")
 
-attach(mydata)
-
-medalcount=function(dataset){
-  g=0
-  s=0
-  b=0
-  
-  for (i in 1:length(dataset)){
-    if(dataset[i]=="Gold"){g=g+1}
-    if(dataset[i]=="Silver"){s=s+1}
-    if(dataset[i]=="Bronze"){b=b+1}
-  }
-  
-  return(c(b,g,s))
-  
-}
-
-bymedalcolorandcountry=tapply(mydata$Medal, mydata$NOC, medalcount)
+bymedalcolorandcountry=tapply(mydata$Medal, mydata$NOC, table)
 
 save(list="bymedalcolorandcountry", file="problem2.RData")
 
